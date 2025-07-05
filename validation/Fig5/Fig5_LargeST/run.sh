ef721508b9a5a8c6ecdcd4674df05c700108ebb8
ST-Balance <cj@hdu.edu.cn>
2025-07-05 17:15:35 +0800

    Update run.sh

--- validation/Fig5/Fig5_LargeST/run.sh
+++ validation/Fig5/Fig5_LargeST/run.sh
@@ -1,4 +1,4 @@
-python main.py --dataset SD --bs 32 --model_name STBalance
+python main.py --dataset SD --bs 32 --model_name STBalance 
 python main.py --dataset GBA --bs 32 --model_name STBalance
 python main.py --dataset GLA --bs 32 --model_name STBalance
 python main.py --dataset CA --bs 8 --model_name STBalance
