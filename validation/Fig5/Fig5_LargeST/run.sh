f4ed6e0de319614606a7112752999e73baf6f845
ST-Balance <cj@hdu.edu.cn>
2025-07-06 11:08:19 +0800

    Update run.sh

--- validation/Fig5/Fig5_LargeST/run.sh
+++ validation/Fig5/Fig5_LargeST/run.sh
@@ -1,4 +1,4 @@
 python main.py --dataset SD --bs 32 --model_name STBalance --reduction 'None' --node_dim 64
-python main.py --dataset GBA --bs 32 --model_name STBalance  --ireduction 'None' --node_dim 64
+python main.py --dataset GBA --bs 32 --model_name STBalance  --reduction 'None' --node_dim 64
 python main.py --dataset GLA --bs 32 --model_name STBalance --reduction 'None' --node_dim 64
 python main.py --dataset CA --bs 8 --model_name STBalance --reduction 'None' --node_dim 64
