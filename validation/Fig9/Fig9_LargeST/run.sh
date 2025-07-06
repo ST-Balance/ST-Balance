d2be939ec7bf27003f8b0ea412d7ab336035eaaa
ST-Balance <cj@hdu.edu.cn>
2025-07-06 10:56:27 +0800

    Update run.sh

--- validation/Fig9/Fig9_LargeST/run.sh
+++ validation/Fig9/Fig9_LargeST/run.sh
@@ -1,4 +1,4 @@
-python main.py --dataset SD --bs 32 --model_name STBalance --if_enhance True --node_dim 64
-python main.py --dataset GBA --bs 32 --model_name STBalance  --if_enhance True --node_dim 64
-python main.py --dataset GLA --bs 32 --model_name STBalance --if_enhance True --node_dim 64
-python main.py --dataset CA --bs 8 --model_name STBalance --if_enhance True --node_dim 64
+python main.py --dataset SD --bs 32 --model_name STBalance --if_enhance True --his_len 672  
+python main.py --dataset GBA --bs 32 --model_name STBalance  --if_enhance True --his_len 1344
+python main.py --dataset GLA --bs 32 --model_name STBalance --if_enhance True --his_len 1344
+python main.py --dataset CA --bs 8 --model_name STBalance --if_enhance True --his_len 1344
